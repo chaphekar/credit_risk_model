73d88f4bd5ba5f49c769f9599ca89273c6175e68
Nilay Borkar <nborkar2005@gmail.com>
2025-11-13 15:56:26 +0530

    removed R files

--- 3_2 Model Selection.R
+++
@@ -1,9 +0,0 @@
-# stats test table
-output$stats_test <- renderGvis({
-  gvisTable(stats.test, options = list(width = "100%"))
-})
-
-# sign table
-output$signs <- renderGvis({
-  gvisTable(sign, options = list(width = "100%"))
-})
